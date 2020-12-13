ea1bdbd2a93e1468eccb56f159a1346ae8683860
SLILAM Anwar <slilamanouar@gmail.com>
2020-12-13 15:00:08 +0100

    initial commit

---
+++ Crud microservice/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_microservice.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dscription { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Crud microservice/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crud_microservice.Models;
+
+namespace Crud_microservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int ProductId);
+        void InsertProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void UpdateProduct(Product Product);
+        void Save();
+    }
+}
